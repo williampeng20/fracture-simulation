9c42ff23941514c50cd1baf907fe62a4e3488011
William Peng <williampeng@Williams-Mac-mini.local>
2019-04-29 19:10:11 -0700

    collision

--- scripts/lattice.py
+++ scripts/lattice.py
@@ -1,17 +1,106 @@
 import bpy
 import mathutils
+import math
 
-class Cube:
+PARTICLE_MASS = 0
+
+def is_inside(p, obj):
+    # max_dist = 1.84467e+19
+    result, point, normal, face = obj.closest_point_on_mesh(p)
+    p2 = point-p
+    v = p2.dot(normal)
+    #print(v)
+    return not(v < 0.0)
+
+class CubeParticle:
     
-    def __init__(self, parent, adj, location, vertices):
-        self.parent_object = parent
-        self.vertices = vertices
+    def __init__(self, parent, location):
+        self.parent = parent
         self.center = location
         self.connections = {}
-        for neighbor in adj:
-            self.connections[neighbor] = 1.0
-        
-class TVertex:
+        self.mass = PARTICLE_MASS
+        #normal corresponds to upwards direction of cube
 
-    def __init__(self, location):
-        self.location = location
+    def add_neighbors(self, neighbors):
+        for neighbor in neighbors:
+            self.connections[neighbor] = 1.0
+
+        
+class SceneObject:
+
+    #Collection of Particles
+
+    def __init__(self, resolution, vertices, render_object):
+        self.v = mathutils.Vector((0,0,0))
+        self.rv = mathutils.Vector((0,0,0))
+        self.a = mathutils.Vector((0,0,-9.8))
+        self.ra = mathutils.Vector((0,0,0))
+
+        self.obj = render_object
+
+        self.vertices = vertices
+
+        self.normal = mathutils.Vector((0,0,1))
+
+        self.particles = []
+        # Assuming 3D quadrilateral geometry
+        x = [co[0] for co in vertices]
+        y = [co[1] for co in vertices]
+        z = [co[2] for co in vertices]
+        width = max(x) - min(x)
+        length = max(y) - min(y)
+        height = max(z) - min(z)
+        self.generate_mesh(width, length, height, min(x), min(y), min(z), resolution)
+        self.res = resolution
+        #self.mass = sum([p.mass for p in self.particles])
+
+        self.width = 0
+        self.height = 0
+        self.depth = 0
+
+    def generate_mesh(self, width, length, height, x, y, z, res):
+        # width = x-axis, length = y-axis, height = z-axis
+        particles = [[[CubeParticle(self, mathutils.Vector((x + i*res, y + j*res, z + k*res))) for k in range(0, math.ceil(height / res))] for j in range(0, math.ceil(length / res))] for i in range(0, math.ceil(width / res))]
+        for i in range(0,len(particles)):
+            for j in range(0,len(particles[0])):
+                for k in range(0,len(particles[0][0])):
+                    neighbors = []
+                    if i-1 in range(0, len(particles)):
+                        neighbors.append(particles[i-1][j][k])
+                    if i+1 in range(0, len(particles)):
+                        neighbors.append(particles[i+1][j][k])
+                    if j-1 in range(0, len(particles[0])):
+                        neighbors.append(particles[i][j-1][k])
+                    if j+1 in range(0, len(particles[0])):
+                        neighbors.append(particles[i][j+1][k])
+                    if k-1 in range(0, len(particles[0][0])):
+                        neighbors.append(particles[i][j][k-1])
+                    if k+1 in range(0, len(particles[0][0])):
+                        neighbors.append(particles[i][j][k+1])
+                    particles[i][j][k].add_neighbors(neighbors)
+                    self.particles.append(particles[i][j][k])
+
+    # Move self to next time frame
+    def move(self):
+        t = 1 / bpy.data.scenes['Scene'].render.fps
+        disp = self.v*t + 0.5*self.a*t**2
+        self.v += self.a*t
+        self.obj.location += disp
+        for vertex in self.vertices:
+            vertex += disp
+        for particle in self.particles:
+            particle.center += disp
+    
+    #Input: List of adjacent particles to split from this scene object
+    #Modifies self to account for splitting objects
+    #Output: Reference to splitted object 
+    def split(self, particles):
+        return
+
+    # Input: SceneObject to detect collision with self
+    # Output: True if collision, False if no collision
+    def detect_collision(self, other_obj):
+        for vertex in self.vertices:
+            if is_inside(vertex, other_obj.obj):
+                return True
+        return False
--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,7 @@
 import bpy
 import mathutils
+import bmesh
+import lattice
 
 # To start from console for console output:
 # "/Applications/Blender/blender.app/Contents/MacOS/blender"
@@ -11,19 +13,67 @@ def clear_scene():
 
 clear_scene()
 
-bpy.ops.mesh.primitive_cube_add(location=(0,0,10))
+scene = bpy.context.scene
 
-bpy.ops.mesh.primitive_plane_add()
-bpy.ops.transform.resize(value=(10,10,10))
+#bpy.ops.mesh.primitive_cube_add(location=(0,0,10))
+cube_vertices = [
+    mathutils.Vector((1.0, 1.0, 9.0)),
+    mathutils.Vector((1.0, -1.0, 9.0)),
+    mathutils.Vector((-1.0, -1.0, 9.0)),
+    mathutils.Vector((-1.0, 1.0, 9.0)),
+    mathutils.Vector((1.0, 1.0, 11.0)),
+    mathutils.Vector((1.0, -1.0, 11.0)),
+    mathutils.Vector((-1.0, -1.0, 11.0)),
+    mathutils.Vector((-1.0, 1.0, 11.0))]
+cube_faces = [
+    (0, 1, 2, 3),
+    (4, 7, 6, 5),
+    (0, 4, 5, 1),
+    (1, 5, 6, 2),
+    (2, 6, 7, 3),
+    (4, 0, 3, 7)]
 
-cube = bpy.data.objects['Cube']
+cube_mesh_data = bpy.data.meshes.new("cube_mesh_data")
+cube_mesh_data.from_pydata(cube_vertices, [], cube_faces)
+cube_mesh_data.update()
+cube_obj = bpy.data.objects.new("Cube", cube_mesh_data)
+scene.objects.link(cube_obj)
 
-v = mathutils.Vector((0,0,0))
-a = mathutils.Vector((0,0,-9.8))
+cube = lattice.SceneObject(0.5, cube_vertices, cube_obj)
+
+bpy.ops.mesh.primitive_cube_add()
+for obj in bpy.context.selected_objects:
+    obj.name = "Plane"
+plane_vertices = [
+    mathutils.Vector((8.0, 8.0, 0.0)),
+    mathutils.Vector((8.0, -8.0, 0.0)),
+    mathutils.Vector((-8.0, -8.0, 0.0)),
+    mathutils.Vector((-8.0, 8.0, 0.0)),
+    mathutils.Vector((8.0, 8.0, 0.5)),
+    mathutils.Vector((8.0, -8.0, 0.5)),
+    mathutils.Vector((-8.0, -8.0, 0.5)),
+    mathutils.Vector((-8.0, 8.0, 0.5))]
+for i in range(0, len(plane_vertices)):
+    bpy.data.objects['Plane'].data.vertices[i].co.x *= 8
+    bpy.data.objects['Plane'].data.vertices[i].co.y *= 8
+    bpy.data.objects['Plane'].data.vertices[i].co.z = bpy.data.objects['Plane'].data.vertices[i].co.z / 2 + 0.5
+
+'''plane_mesh_data = bpy.data.meshes.new("plane_mesh_data")
+plane_mesh_data.from_pydata(plane_vertices, plane_edges, plane_faces)
+plane_mesh_data.update()
+plane_obj = bpy.data.objects.new("Plane", plane_mesh_data)
+scene.objects.link(plane_obj)'''
+
+plane = lattice.SceneObject(0.5, plane_vertices, bpy.data.objects['Plane'])
+
+#v = mathutils.Vector((0,0,0))
+#a = mathutils.Vector((0,0,-9.8))
 for f in range(0,100):
-    t = f / bpy.data.scenes['Scene'].render.fps
-    cube.location += v*t + 0.5*a*t**2
-    cube.keyframe_insert(data_path="location", frame=f)
+    cube.move()
+    cube.obj.keyframe_insert(data_path="location", frame=f)
+
+    if cube.detect_collision(plane):
+        break
     
 
 
